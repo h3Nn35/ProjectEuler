a966971480b14b0e505431ceba83e1ceafcb03f4
Christian Hennes <h3nn35@pm.me>
2022-05-16 14:21:58 +0200

    -

---
+++ Double-base_Palindromes/mathefunktionen.h
@@ -0,0 +1,17 @@
+#ifndef MATHE_H
+#define MATHE_H
+#include <iostream>
+
+using namespace std;
+
+
+int factorial(int x);
+bool istPalindrome(string kandidat);
+bool istPalindrome(int kandidatalszahl);
+string toBinary(int n);
+long naechstePrimzahl(long letzteZahl);
+int naechstePrimzahl(int Startzahl);
+bool istPrimzahl(long zahl);
+bool istPrimzahl(int zahl);
+
+#endif
