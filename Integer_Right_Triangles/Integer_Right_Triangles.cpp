25b6919cdf1b5c682327a5ec0b73a92e6c65de2a
Christian Hennes <h3nn35@pm.me>
2022-05-18 17:47:32 +0200

    Integer_Right_Triangles_final

--- Integer_Right_Triangles/Integer_Right_Triangles.cpp
+++ Integer_Right_Triangles/Integer_Right_Triangles.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-
-
+    int a, b, c, anzahl, max = 0, spitzenreiter = 0;
+    double temp;
+    for (int x = 1; x < 1001; x++) {
+        anzahl = 0;
+        for (int i = 1; i < 998; i++) {
+            a = i;
+            for (int j = a; j < 998; j++) {
+                b = j;
+                temp = sqrt(a * a + b * b);
+                if (temp - floor(temp) != 0 || a + b + temp != x)
+                    continue;
+                else
+                    anzahl++;
+            }
+        }
+        if (anzahl > max){
+            max = anzahl;
+            spitzenreiter = x;
+        }
+    }
+    cout << spitzenreiter << endl;
     return EXIT_SUCCESS;
 }
