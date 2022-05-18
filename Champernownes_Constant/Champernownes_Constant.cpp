1a24a2324c4497cfcf54906929b8199cf8d1ce0e
Christian Hennes <h3nn35@pm.me>
2022-05-18 21:32:26 +0200

    Champernownes_Constant_final

--- Champernownes_Constant/Champernownes_Constant.cpp
+++ Champernownes_Constant/Champernownes_Constant.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
-
-
+int main() {
+    int result = 1;
+    string constant = "0.";
+    for (int i = 1; i < 1000000; i++)
+        constant.append(to_string(i));
+    for (int x = 1; x <= 1000000; x *= 10){
+        string temp = constant.substr(x + 1, 1);
+        result *= stoi(temp);
+    }
+    cout << result << endl;
     return EXIT_SUCCESS;
 }
