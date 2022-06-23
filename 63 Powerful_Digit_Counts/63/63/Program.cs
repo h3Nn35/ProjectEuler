208aceec1a430415b43cd158345633b6c5116090
Christian Hennes <h3nn35@pm.me>
2022-06-23 21:46:19 +0200

    97

---
+++ 63 Powerful_Digit_Counts/63/63/Program.cs
@@ -0,0 +1,12 @@
+﻿int result = 0;
+int lower = 0;
+int n = 1;
+
+while (lower < 10)
+{
+    lower = (int)Math.Ceiling(Math.Pow(10, (n - 1.0) / n));
+    result += 10 - lower;
+    n++;
+}
+
+Console.WriteLine(n);
---
+++ 92 Square digits chains/Square_digit_chains/Square_digit_chains/Program.cs
@@ -0,0 +1,21 @@
+﻿int zahl, ziffer, anzahl = 0;
+string kandidat;
+
+for (int i = 2; i < 10000000; i++)
+{
+    zahl = i;
+    while (zahl != 1 && zahl != 89)
+    {
+        kandidat = zahl.ToString();
+        zahl = 0;
+        for (int j = 0; j < kandidat.Length; j++)
+        {
+            ziffer = int.Parse(kandidat.Substring(j, 1));
+            zahl += ziffer * ziffer;
+        }
+    }
+    if (zahl == 89)
+        anzahl++;
+}
+
+Console.WriteLine(anzahl);
